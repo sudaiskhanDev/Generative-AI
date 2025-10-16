039670c4ffca8b17a6ef3d82fd76d8a073611c92
sudaiskhanDev <sudaisliveofficial@gmail.com>
2025-10-16 20:00:30 +0500

    bg

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-      <div className="main-nav bg-[#ffffff] w-[90%] max-w-[400px] rounded-2xl mx-auto shadow-md overflow-hidden text-black flex justify-between items-center p-2">
+      <div className="main-nav bg-[#ffffff] h-9 w-[90%] max-w-[400px] rounded-2xl mx-auto shadow-md overflow-hidden text-black flex justify-between items-center p-2">
         <div className="logo">
           <img src='/logo.png' alt="logo" className='w-10 h-10 object-contain'/>
         </div>
-        <div className="menu font-bold text-lg hover:text-gray-600 transition-colors">Menu</div>
+        <div className="menu font-bold text-lg hover:text-blue-600 transition-colors cursor-pointer">Menu</div>
       </div>
     </>
   )
